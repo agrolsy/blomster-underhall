08fb75e75a40db4aa0b0bd67ca7a3068177e6da8
agrolsy <elsa@blomsterhundar.se>
2026-07-30 23:11:47 +0200

    Configure mower blade entities

--- custom_components/blomster_maintenance/config_flow.py
+++ custom_components/blomster_maintenance/config_flow.py
@@ -5,7 +5,15 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.helpers import config_validation as cv
 
-from .const import CONF_WATER_INSTALLATION_DATE, CONF_WATER_SOURCE_ENTITY, DOMAIN
+from .const import (
+    CONF_BLADE_INTERVAL_HOURS,
+    CONF_BLADE_USAGE_ENTITY,
+    CONF_BLADE_WARNING_ENTITY,
+    CONF_WATER_INSTALLATION_DATE,
+    CONF_WATER_SOURCE_ENTITY,
+    DEFAULT_BLADE_INTERVAL_HOURS,
+    DOMAIN,
+)
 
 
 class BlomsterMaintenanceConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -21,6 +29,18 @@ class BlomsterMaintenanceConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             {
                 vol.Required(CONF_WATER_SOURCE_ENTITY): cv.entity_id,
                 vol.Required(CONF_WATER_INSTALLATION_DATE): cv.date,
+                vol.Required(
+                    CONF_BLADE_USAGE_ENTITY,
+                    default="sensor.garden_hugo_ii_luba_vpqnssl9_bladanvandningstid",
+                ): cv.entity_id,
+                vol.Required(
+                    CONF_BLADE_WARNING_ENTITY,
+                    default="sensor.garden_hugo_ii_luba_vpqnssl9_bladslitagevarningstid",
+                ): cv.entity_id,
+                vol.Required(
+                    CONF_BLADE_INTERVAL_HOURS,
+                    default=DEFAULT_BLADE_INTERVAL_HOURS,
+                ): vol.All(vol.Coerce(float), vol.Range(min=1)),
             }
         )
         return self.async_show_form(step_id="user", data_schema=schema)
